8f44d238796edafa6b520a8c7898cb8609ecb178
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-08 11:30:35 +0800

    feat: 添加异常处理类

---
+++ src/main/java/com/code/cong/interview/exception/BusinessException.java
@@ -0,0 +1,34 @@
+package com.code.cong.interview.exception;
+
+
+import com.code.cong.interview.common.ErrorCode;
+import lombok.Getter;
+
+/**
+ * 自定义异常类
+ * # @author <a href="https://github.com/lhccong">程序员聪</a>
+ */
+@Getter
+public class BusinessException extends RuntimeException {
+
+    /**
+     * 错误码
+     */
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+
+}
---
+++ src/main/java/com/code/cong/interview/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.code.cong.interview.exception;
+
+import com.code.cong.interview.common.BaseResponse;
+import com.code.cong.interview.common.ErrorCode;
+import com.code.cong.interview.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ * # @author <a href="https://github.com/lhccong">程序员聪</a>
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
