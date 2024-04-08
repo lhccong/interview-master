b67e36ba1fd539195d2a9330903c18a9f215cfc3
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-08 15:12:30 +0800

    feat: 新增系统常量

---
+++ src/main/java/com/code/cong/interview/constant/SystemConstants.java
@@ -0,0 +1,25 @@
+package com.code.cong.interview.constant;
+
+/**
+ * 系统常量
+ * @author 聪
+ * @date 2023/09/11
+ */
+public interface SystemConstants {
+
+    /**
+     * 用户登录态键
+     */
+    String USER_LOGIN_STATE = "user_login";
+
+    /**
+     * 全局盐值
+     */
+    String SALT = "cong";
+
+    /**
+     * 系统群聊房间 id
+     */
+    Long SYSTEM_ROOM_ID = 1L;
+}
+
