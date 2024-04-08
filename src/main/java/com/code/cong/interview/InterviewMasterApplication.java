bd143680203f664660c37c93b76c597e93ece656
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-08 11:15:53 +0800

    init：面试大师初始化，引入依赖和 README 文档

---
+++ src/main/java/com/code/cong/interview/InterviewMasterApplication.java
@@ -0,0 +1,13 @@
+package com.code.cong.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/cong/interview/InterviewMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.cong.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
