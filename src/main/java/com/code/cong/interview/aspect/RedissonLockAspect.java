08703e25b68b9c0397bf9c20be761ec307138867
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-08 11:40:03 +0800

    feat: 引入分布式锁切面

---
+++ src/main/java/com/code/cong/interview/aspect/RedissonLockAspect.java
@@ -0,0 +1,40 @@
+package com.code.cong.interview.aspect;
+
+
+import cn.hutool.core.text.CharSequenceUtil;
+import com.code.cong.interview.annotation.RedissonLock;
+import com.code.cong.interview.service.impl.LockService;
+import com.code.cong.interview.utils.SpElUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.lang.reflect.Method;
+
+/**
+ * Description: 分布式锁切面
+ * @author liuhuaicong
+ * @date 2023/10/18
+ */
+@Slf4j
+@Aspect
+@Component
+@Order(0)//确保比事务注解先执行，分布式锁在事务外
+public class RedissonLockAspect {
+    @Resource
+    private LockService lockService;
+
+    @Around("@annotation(com.code.cong.interview.annotation.RedissonLock)")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        RedissonLock redissonLock = method.getAnnotation(RedissonLock.class);
+        String prefix = CharSequenceUtil.isBlank(redissonLock.prefixKey()) ? SpElUtils.getMethodKey(method) : redissonLock.prefixKey();//默认方法限定名+注解排名（可能多个）
+        String key = SpElUtils.parseSpEl(method, joinPoint.getArgs(), redissonLock.key());
+        return lockService.executeWithLockThrows(prefix + ":" + key, redissonLock.waitTime(), redissonLock.unit(), joinPoint::proceed);
+    }
+}
